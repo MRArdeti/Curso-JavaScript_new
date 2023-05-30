7a99ac7be04394b0ae31fcd0a8fec8fe07504f5e
Rosario Ardeti <rosarioardeti15@gmail.com>
2023-05-29 22:07:40 -0300

    Segundo ejercicio

---
+++ Lista.js
@@ -0,0 +1,8 @@
+const fecha = new Date(1998, 7, 15).getTime();
+const interpretacionSuenos = { 
+    autor: "Sigmund Freud",
+    anio: 1999
+ };
+const lista = ["Maria del Rosario", 24, false, fecha, interpretacionSuenos];
+
+console.log(lista);
