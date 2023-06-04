5b01f3e6890c7ebf5922b4fbc89bf52b04c028fc
Rosario Ardeti <rosarioardeti15@gmail.com>
2023-06-03 22:52:57 -0300

    Mejore el ejercicio de la lista

--- Lista.js
+++ Lista.js
@@ -1,8 +1,10 @@
-const fecha = new Date(1998, 7, 15).getTime();
-const interpretacionSuenos = { 
+const fecha = new Date(1998, 7, 15).toLocaleDateString(`es-AR`);
+const libro = { 
+    titulo: "La interpretacion de los sueños",
     autor: "Sigmund Freud",
-    anio: 1999
+    anio: 1900,
+    url: "https://www.google.com.ar/books/edition/La_interpretaci%C3%B3n_de_los_sue%C3%B1os/FUIiAgAAQBAJ?hl=es&gbpv=1&printsec=frontcover"
  };
-const lista = ["Maria del Rosario", 24, false, fecha, interpretacionSuenos];
+const lista = ["Maria del Rosario", 24, false, fecha, libro];
 
 console.log(lista);
